bd116a8d9fabc8b748d3e01486965e20c8d3223d
Catiusci Scheffer <cpcscheffer@outlook.com>
2023-01-29 19:38:18 -0300

    url json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = './data/profile.json';
+    const url = "https://github.com/CatiusciScheffer/Portifolio/blob/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
